eb9f0c3800547ab8609832350434c77f6b79db22
vipul-hedaoo <78016931+vipul-hedaoo@users.noreply.github.com>
2026-03-10 12:13:31 +0530

    Add files via upload

---
+++ milagecalculator.cpp
@@ -0,0 +1,15 @@
+# include<iostream>
+using namespace std;
+int main()
+{
+    int milage=0,traveldistance=450,fule=30;
+    milage=traveldistance/fule;
+    milage>=15?cout<<milage<<" Efficient"<<endl:cout<<milage<<" Not Efficient"<<endl;
+    // if(milage>=15)
+    // {
+    //     cout<<milage<<" Efficient";
+    // }
+    // else{
+    //     cout<<milage<<" Not Efficient";
+    // }
+}

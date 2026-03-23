f7a61d945764222ea96494a10fec88a021eea381
vipul-hedaoo <78016931+vipul-hedaoo@users.noreply.github.com>
2026-03-23 07:36:14 +0530

    Add files via upload

---
+++ stringmultiply.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+string multiply(string num1, string num2) {
+   if(num1=="0" && num2=="0") return "0";
+   int n=num1.size(),m=num2.size();
+   vector<int> result(n+m,0);
+   for(int i=n-1;i>=0;i--)
+   {
+    for(int j=m-1;j>=0;j--)
+    {
+        int mul=(num1[i]-'0')*(num2[j]-'0');
+        int sum=mul+result[i+j+1];
+        result[i+j+1]=sum%10;
+        result[i+j]+=sum/10;
+    }
+   }
+
+   string ans="";
+   for(int nums : result)
+   {
+    if(!(ans.empty() && nums==0))
+    { 
+        ans += to_string(nums);
+    }
+   }
+   return ans.empty()?0:ans;
+}
+int main()
+{
+    string a="123";
+    string b="456";
+    cout<<multiply(a,b);
+}

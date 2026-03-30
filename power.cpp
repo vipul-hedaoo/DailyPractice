5346e8623bdb5a19a28f729e7289e5128dae414d
vipul-hedaoo <78016931+vipul-hedaoo@users.noreply.github.com>
2026-03-30 22:31:41 +0530

    Refactor power calculation and restore main logic

--- power.cpp
+++ power.cpp
@@ -6,28 +6,24 @@ double simplepow(double number,int power)
     return pow(number,power);
 }
 int main() {
-//     double num = 2.0;
-//     int power = -2;
-//     double result = 1.0;
-//     int p = power;
-
-//     // Handle negative power
-//     if (p < 0) {
-//         p = -p;
-//     }
-
-//     // Calculate power
-//     for (int i = 0; i < p; i++) {
-//         result *= num;
-//     }
-
-//     // If original power was negative
-//     if (power < 0) {
-//         result = 1 / result;
-//     }
-
-//     cout << result;
- cout<<simplepow(2,-2);
+    double num = 2.0;
+    int power = -2;
+    double result = 1.0;
+    int p = power;
+    // Handle negative power
+    if (p < 0) {
+        p = -p;
+    }
+    // Calculate power
+    for (int i = 0; i < p; i++) {
+        result *= num;
+    }
+    // If original power was negative
+    if (power < 0) {
+        result = 1 / result;
+    }
+    cout << result;
+ //cout<<simplepow(2,-2);
  }
 
 

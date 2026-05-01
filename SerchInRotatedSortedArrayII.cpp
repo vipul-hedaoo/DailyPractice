d3dd89aa4ac1932d25274cfcda9e522944a2fe87
vipul-hedaoo <78016931+vipul-hedaoo@users.noreply.github.com>
2026-05-01 22:45:05 +0530

    Add files via upload

---
+++ SerchInRotatedSortedArrayII.cpp
@@ -0,0 +1,17 @@
+//searchInRotatedSortedArrayII
+#include<iostream>
+#include<vector>    
+using namespace std;
+int main()
+{
+    vector<int> arr={1,2,3,4,5,6,7,8};
+    for(int i=0;i<arr.size();i++)
+    {
+        If(arr[i]==traget)
+        {
+            Cout<<"True";
+            // return true;
+        }
+        cout<<"False";
+        // return flase;
+}
